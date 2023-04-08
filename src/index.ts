583d893740f3a1a487518685a05c4f85f8e82513
rawr111 <70286254+rawr111@users.noreply.github.com>
2023-04-08 14:26:59 +0300

    (fix) doRequest

--- src/index.ts
+++ src/index.ts
@@ -138,10 +138,18 @@ class RequestBase {
                 }
             }).catch(err => {
                 if (err.response)
-                    return { headers: err.response.headers, body: err.response.body, statusCode: err.statusCode, requestOptions: actualRequestOptions };
+                    if (options?.isJsonResult === true || typeof (options?.isJsonResult) === 'undefined') {
+                        try {
+                            return { headers: err.response.headers, body: JSON.parse(err.response.body), statusCode: err.statusCode, requestOptions: actualRequestOptions };
+                        } catch (err){
+                            throw new Error(`Cant parse response. It's not in json format`);
+                        }
+                    } else {
+                        return { headers: err.response.headers, body: err.response.body, statusCode: err.statusCode, requestOptions: actualRequestOptions };
+                    }
                 else {
-                    throw new Error(err);
-                }
+                        throw new Error(err);
+                    }
             });
             return result;
         } catch (err) {
